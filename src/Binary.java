93e4385a19e5422ac86cbe7843c15425dc65620f
rajat mittal <rajatmittal603@gmail.com>
2015-07-10 17:01:36 +0530

    test 10/07/15

---
+++ src/Binary.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by rajat mittal on 7/10/2015.
+ */
+public class Binary {
+    public static void main(String[] args) {
+
+
+            Integer i = new Integer(29);
+            String binary = Integer.toBinaryString(i);
+            System.out.println("Binary value: "+binary);
+
+            }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by rajat mittal on 7/10/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        int n=0;
+        String result=null;
+        for(int i=0;i<=n;i++)
+        {
+            if(n%2==0){
+                System.out.print(n%2);
+                result=n%2+result;
+            }
+            else{
+                System.out.print(n%2);
+                result=n%2+result;
+            }
+        }
+    }
+}
